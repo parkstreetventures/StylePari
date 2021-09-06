1dbc9cc41da21c60cf71a9797b1bb8b508804734
Subu Sangameswar <sangames@duggl.com>
2021-09-05 20:30:27 -0400

    reinitialized repo

--- image_download.py
+++ image_download.py
@@ -4,14 +4,14 @@
 import urllib.request
 import pandas as pd
 
-df = pd.read_csv ('/home/ec2-user/project-python/StylePari/data/image_list.csv')
+df = pd.read_csv ('./data/image_list.csv')
 df.head()
 
 # let us experiment downloading a small set of images
-small_dataset = df.head(50)
+small_dataset = df.head(5)
 small_dataset
 
-file_name_old = "/home/ec2-user/project-python/StylePari/images/download_filename_"
+file_name_old = "./images/download_filename_"
 for ind in small_dataset.index:
      imgURL = small_dataset['Image Src'][ind]
      file_name = file_name_old + str(ind) + ".jpg"
