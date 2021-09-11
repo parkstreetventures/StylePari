b2d6ed5162560fbf298d7072cc11cf7be844c8ad
Subu Sangameswar <sangames@duggl.com>
2021-09-11 12:25:50 -0400

    updated files

--- fashion_recommender_v3.py
+++ fashion_recommender_v3.py
@@ -10,6 +10,7 @@ import streamlit.components.v1 as stc
 
 
 # Load EDA
+import numpy as np
 import pandas as pd 
 #from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer 
@@ -40,6 +41,58 @@ PARSED_PATH = "./data/saree_parsed_new.csv"
 TFIDF_ENCODING_PATH = "./model/saree_tfidf_encodings.pkl"
 TFIDF_MODEL_PATH = "./model/saree_tfidf.pkl"
 
+# color functionality 
+
+import re
+re_color = re.compile('#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})')
+from math import sqrt
+
+def color_to_rgb(color):
+    return tuple(int(x, 16) / 255.0 for x in re_color.match(color).groups())
+
+def similarity(color1, color2):
+    """Computes the pearson correlation coefficient for two colors. The result
+    will be between 1.0 (very similar) and -1.0 (no similarity)."""
+    c1 = color_to_rgb(color1)
+    c2 = color_to_rgb(color2)
+
+    s1 = sum(c1)
+    s2 = sum(c2)
+    sp1 = sum(map(lambda c: pow(c, 2), c1))
+    sp2 = sum(map(lambda c: pow(c, 2), c2))
+    sp = sum(map(lambda x: x[0] * x[1], zip(c1, c2)))
+
+    try:
+            computed = (sp - (s1 * s2 / 3.0)) / sqrt((sp1 - pow(s1, 2) / 3.0) * (sp2 - pow(s2, 2) / 3.0))
+    except:
+            computed = 0
+    
+    return computed
+
+color_names = {
+    '#000000': 'black',
+    '#ffffff': 'white',
+    '#808080': 'dark gray',
+    '#b0b0b0': 'light gray',
+    '#ff0000': 'red',
+    '#800000': 'dark red',
+    '#00ff00': 'green',
+    '#008000': 'dark green',
+    '#0000ff': 'blue',
+    '#000080': 'dark blue',
+    '#ffff00': 'yellow',
+    '#808000': 'olive',
+    '#00ffff': 'cyan',
+    '#ff00ff': 'magenta',
+    '#800080': 'purple'
+    }
+
+def find_name(color):
+    sim = [(similarity(color, c), name) for c, name in color_names.items()]
+    return max(sim, key=lambda x: x[0])[1]
+
+
+# end color functionality
 
 # debugging help file
 def showStatus(moduleName):
@@ -248,7 +301,7 @@ def complementaryColor(color_choice):
     elif color_choice=="orange":
         return "yellow, red"
     else:
-        return "white"
+        return "black"
 
 
 #def letRecommend(search_term, number_of_rec):
@@ -277,6 +330,7 @@ def write_results(results):
                 st.write(score)
 
 
+
 def main():
 
     st.title("fashion recommender v3")
@@ -293,13 +347,17 @@ def main():
     elif choice == "Recommend":
         st.subheader("Recommend")
         #search_term = st.text_input("Search")
+
         menu_color = ["red","blue","violet","green","pink", "indigo", "yellow", "orange", "white"]
-        color_choice = st.sidebar.selectbox("Color", menu_color)
+        #color_choice = st.sidebar.selectbox("Color", menu_color)
+        color_choice_2 = st.sidebar.color_picker("pick a color")
         menu_fabric = ["linen", "silk", "cotton"]
         fabric_choice = st.sidebar.selectbox("fabric", menu_fabric)
         #num_of_rec = st.sidebar.number_input("Number",1,10,7)
-        if st.button("Recommend"):
+        if st.sidebar.button("Recommend"):
             #letRecommend(search_term, num_of_rec)
+            color_choice = find_name(color_choice_2)
+            st.write("color choice: " + color_choice)
             letRecommend(color_choice, fabric_choice)
 
     else:
